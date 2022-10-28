f8c18362768b69224b5fd71cdbd94ba929e5cca1
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-10-28 21:14:32 +0200

    task first try

---
+++ src/js/fetch.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+export default class ImageApiServise {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getPictures() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '30861394-45e6fcfd438676dc717df7503';
+    const options = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: this.page,
+    });
+    const response = await axios.get(`${BASE_URL}?${options}`);
+    return response.data.hits;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
---
+++ src/js/search_pictures.js
@@ -0,0 +1,54 @@
+import ImageApiServise from './fetch';
+import Notiflix from 'notiflix';
+import galleryTamplate from '../tamplates/gallery.hbs';
+
+const refs = {
+  formEl: document.querySelector('#search-form'),
+  gelleryEl: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
+};
+
+const imageApiServise = new ImageApiServise();
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.loadMore.addEventListener('click', onLoadMore);
+
+async function onFormSubmit(e) {
+  e.preventDefault();
+  imageApiServise.query = e.currentTarget.elements.searchQuery.value;
+  imageApiServise.resetPage();
+
+  imageApiServise.getPictures().then(resp => {
+    if (resp.ok) {
+      throw new Error(resp.status);
+    } else if (resp.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    } else {
+      galleryRemove();
+      galleryMarkUP(resp);
+      refs.loadMore.classList.remove('hidden');
+      imageApiServise.incrementPage();
+    }
+  });
+}
+
+function onLoadMore(e) {
+  imageApiServise.getPictures().then(resp => {
+    if (resp.ok) {
+      throw new Error(resp.status);
+    } else {
+      galleryMarkUP(resp);
+    }
+  });
+}
+
+function galleryMarkUP(images) {
+  refs.gelleryEl.insertAdjacentHTML('beforeend', galleryTamplate(images));
+}
+
+function galleryRemove() {
+  refs.gelleryEl.innerHTML = '';
+}
